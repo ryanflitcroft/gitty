7cdee3aa3bfd1b7f791e7f9ee9eed06edb1a0dfd
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-03-31 18:42:54 -0700

    app route setup, controller github route get /login redirects to github oauth, test written and passing.

---
+++ __tests__/github.test.js
@@ -0,0 +1,22 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('gitty routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('should redirect to GitHub OAuth page on signup/signin', async () => {
+    const res = await request(app).get('/api/v1/github/login');
+
+    expect(res.header.location).toMatch(
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
+    );
+  });
+});
