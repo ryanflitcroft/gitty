026d4f1c17d346682867e67ec2e4793bec47c0c8
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-04-08 11:49:19 -0700

    test quotes written for route get /quotable/:count

--- __tests__/quotes.test.js
+++ __tests__/quotes.test.js
@@ -43,7 +43,7 @@ describe('routes for quotes', () => {
     ]);
   });
 
-  it.only('should be able to get a list of quotes from futuramaapi, where the length of the list is dependent on the url params given', async () => {
+  it('should be able to get a list of quotes from futuramaapi, where the length of the list is dependent on the url params given', async () => {
     let res = await request(app).get('/api/v1/quotes/futurama/1');
     expect(res.body).toEqual([
       {
@@ -62,6 +62,16 @@ describe('routes for quotes', () => {
         author: expect.any(String),
         description: expect.any(String),
       },
+      {
+        author: expect.any(String),
+        description: expect.any(String),
+      },
+    ]);
+  });
+
+  it.only('should be able to get a list of quotes from futuramaapi, where the length of the list is dependent on the url params given', async () => {
+    const res = await request(app).get('/api/v1/quotes/quotable/1');
+    expect(res.body).toEqual([
       {
         author: expect.any(String),
         description: expect.any(String),
